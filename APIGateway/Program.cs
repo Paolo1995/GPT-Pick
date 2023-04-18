2c65da51750e982a9f3d46031917e0880532e48c
constantinpaolo <constantinpaolo95@gmail.com>
2023-04-19 00:37:44 +0300

    a

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Gateway", Version = "v1" });
+});
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -13,12 +22,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Gateway v1");
+    });
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseOcelot().Wait();
 
 app.MapControllers();
 
